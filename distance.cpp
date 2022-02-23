41f2fb9ab58a88c39dd16bf4fc68cb337505158d
Pallavi-Mali <98145856+Pallavi-Mali@users.noreply.github.com>
2022-02-23 23:04:18 +0530

    Add files via upload

---
+++ distance.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+{
+public:
+int feet, inch;
+distanceD1( f, i)
+{
+feet = f;
+inch = i;
+cout << "feet:" << feet << "inches:" << inches << endl;
+}
+};
+int main()
+{
+int f, i;
+cout << "enter feets and inches:" <<endl;
+cin >> f >> i;
+distanceD1(f,i)
+return 0;
+}
---
+++ square.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int i;
+cout<<"This is output\n\n";
+cout<<"Enter a number";
+cin>>i;
+cout << i << "squared is " << i*i << "\n" ;
+return 0;
+}
