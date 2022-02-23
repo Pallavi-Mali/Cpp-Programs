57c39179f33af5bd497a363f4edabf1b52fa78b1
Pallavi-Mali <98145856+Pallavi-Mali@users.noreply.github.com>
2022-02-23 11:14:31 +0530

    Add files via upload

---
+++ cube.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+inline int cube(int a)
+{
+return a*a*a;
+}
+int main()
+{
+cout<<"Cube of 3 is :";
+cout<<3*3*3<<endl<<endl;
+return 0;
+}
---
+++ overload_operator7.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+class Complex{
+int real,img;
+public:
+Complex(){
+real=0;
+img=0;
+}
+Complex(int x, int y){
+real = x;
+img = y;
+}
+void display(){
+cout<<"The value for real part : "<< real << " and img part: "<<img<<endl<<endl;
+}
+Complex operator >= (Complex obj)
+{
+Complex temp;
+temp.real = real >= obj.real;
+temp.img = img >= obj.img;
+return temp;
+}
+};
+
+
+int main(){
+Complex c1(10,12), c2(5,10);
+Complex c3;
+c1.display();
+c2.display();
+c3.display();
+cout<<"The action starts here :"<<endl;
+c3= c1>=c2;
+c3.display();
+
+return 0;
+}
