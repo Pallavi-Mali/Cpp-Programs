3747f7b4db4185c429fe802b508443816f06141c
Pallavi-Mali <98145856+Pallavi-Mali@users.noreply.github.com>
2022-02-23 23:12:41 +0530

    Add files via upload

---
+++ unary_op3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class demo{
+int m;
+public:
+demo(){m=0;}
+demo(int x){
+m=x;
+}
+
+void operator ++(){
+++m;
+cout<<"Pre Increment ="<<m;
+}
+void operator ++(int){
+m++;
+cout<<"Post Increment="<<m;
+}
+};
+int main(){
+demo d1(5);
+++d1;
+d1++;
+}
